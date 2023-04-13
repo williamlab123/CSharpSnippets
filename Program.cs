4895b98becbd40a9dadc7e75e6559111477f7558
William Pedro <54008790+williamlab123@users.noreply.github.com>
2023-04-13 17:21:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+      
+        Console.WriteLine("Array desordenado:");
+        PrintArray(arr);
+
+        BubbleSort(arr);
+
+        Console.WriteLine("\nArray ordenado:");
+        PrintArray(arr);
+    
+
+   
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        bool swapped;
+        do
+        {
+            swapped = false;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            n--;
+        } while (swapped);
+    }
+
+     static void PrintArray(int[] arr)
+    {
+        foreach (int i in arr)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+    //Bubblesort to ordenate an array
+    
+
