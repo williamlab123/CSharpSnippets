a047652fa702a8ab67ee7f755a2726252516e880
William Pedro <54008790+williamlab123@users.noreply.github.com>
2023-04-14 15:27:32 -0300

    Update LevenshteinDistanceAlgorithm.cs

--- LevenshteinDistanceAlgorithm.cs
+++ LevenshteinDistanceAlgorithm.cs
@@ -1,95 +1,91 @@
- public static string LevenshteinDistance(string _brand, int choose)
+using System;
+
+class CorretorOrtografico
+{
+    static void Main()
+    {
+        string[] dicionario = { "casa", "carro", "comida", "caneca", "celular" };
+
+        Console.WriteLine("Digite uma palavra:");
+        string palavra = Console.ReadLine();
+
+        if (!Array.Exists(dicionario, x => x == palavra))
         {
+            Console.WriteLine("Distância máxima de edição:");
+            int distanciaMaxima = int.Parse(Console.ReadLine());
 
-            string[] marcas = {
-                   ""
-        };
-        
-        string palavra = _brand;
+            string corrigida = CorrigirPalavra(palavra, dicionario, distanciaMaxima);
 
-
-                if(marcas.Contains(palavra))
-                {
-                 return palavra;
-                }
-
-                if (!marcas.Contains(palavra))
-                {
-                    int distanciaMaxima = 4;
-                    string corrigida = null;
-                    int menorDistancia = int.MaxValue;
-
-                    foreach (string marca in marcas)
-                    {
-                        int distancia = DistanciaLevenshtein(palavra, marca);
-                        if (distancia <= distanciaMaxima && distancia < menorDistancia)
-                        {
-                            corrigida = marca;
-                            menorDistancia = distancia;
-                        }
-                    }
-
-                    if (corrigida != null)
-                    {
-
-
-                        Console.Write("Você quis dizer ");
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        System.Console.Write(corrigida + " ?");
-                        Console.ResetColor();
-                        ConsoleKeyInfo keyInfo = Console.ReadKey();
-                        if (keyInfo.Key == ConsoleKey.Enter)
-                        {
-                            return corrigida;
-                        }
-
-                        else
-                        {
-
-                            return corrigida = null;
-                        }
-
-
-
-                    }
-                    else
-                    {
-
-                        return corrigida = null;
-                    }
-                }
-                else
-                {
-
-                    return "";
-                }
-                
-                static int DistanciaLevenshtein(string s, string t)
-        {
-            int[,] d = new int[s.Length + 1, t.Length + 1];
-
-            for (int i = 0; i <= s.Length; i++)
+            if (corrigida != null)
             {
-                d[i, 0] = i;
+                Console.WriteLine("Você quis dizer \"{0}\"?", corrigida);
             }
-
-            for (int j = 0; j <= t.Length; j++)
+            else
             {
-                d[0, j] = j;
+                Console.WriteLine("Palavra não encontrada no dicionário.");
             }
-
-            for (int j = 1; j <= t.Length; j++)
-            {
-                for (int i = 1; i <= s.Length; i++)
-                {
-                    int custo = (s[i - 1] == t[j - 1]) ? 0 : 1;
-
-                    d[i, j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + custo);
-                }
-            }
-
-            return d[s.Length, t.Length];
         }
+        else
+        {
+            Console.WriteLine("Palavra correta.");
+        }
+
+        Console.ReadLine();
+    }
+
+    static string CorrigirPalavra(string palavra, string[] dicionario, int distanciaMaxima)
+    {
+        int distanciaMinima = int.MaxValue;
+        string melhorCorrecao = null;
+
+        foreach (string palavraCorreta in dicionario)
+        {
+            int distancia = DistanciaLevenshtein(palavra, palavraCorreta);
+
+            if (distancia <= distanciaMaxima && distancia < distanciaMinima)
+            {
+                distanciaMinima = distancia;
+                melhorCorrecao = palavraCorreta;
+            }
+        }
+
+        if (distanciaMinima <= distanciaMaxima && melhorCorrecao != null)
+        {
+            return melhorCorrecao;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    static int DistanciaLevenshtein(string s, string t)
+    {
+        int[,] d = new int[s.Length + 1, t.Length + 1];
+
+        for (int i = 0; i <= s.Length; i++)
+        {
+            d[i, 0] = i;
+        }
+
+        for (int j = 0; j <= t.Length; j++)
+        {
+            d[0, j] = j;
+        }
+
+        for (int j = 1; j <= t.Length; j++)
+        {
+            for (int i = 1; i <= s.Length; i++)
+            {
+                int custo = (s[i - 1] == t[j - 1]) ? 0 : 1;
+
+                d[i, j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + custo);
+            }
+        }
+
+        return d[s.Length, t.Length];
+    }
+}
 
 
 /*The Levenshtein algorithm, also known as the Levenshtein distance or edit distance algorithm, is a method for calculating the minimum number
