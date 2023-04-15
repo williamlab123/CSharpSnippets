eb7ae494189a02ce8c608d89350537bd0c2bf927
William Pedro <54008790+williamlab123@users.noreply.github.com>
2023-04-14 23:18:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using QRCoder;
+using System;
+using System.IO;
+
+class QRGenerator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira o texto a ser codificado:");
+        string inputText = Console.ReadLine();
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(inputText, QRCodeGenerator.ECCLevel.Q);
+        AsciiQRCode qrCode = new AsciiQRCode(qrCodeData);
+        string qrCodeAscii = qrCode.GetGraphic(1);
+        Console.WriteLine(qrCodeAscii);
+
+        // Adicionando a funcionalidade de marcar o QR code como lido
+        string qrCodeFile = "qrCodeRead.txt";
+        if (File.Exists(qrCodeFile))
+        {
+            using (StreamWriter sw = File.AppendText(qrCodeFile))
+            {
+                sw.WriteLine(inputText);
+            }
+        }
+        else
+        {
+            using (StreamWriter sw = File.CreateText(qrCodeFile))
+            {
+                sw.WriteLine(inputText);
+            }
+        }
+        Console.WriteLine("QR Code marcado como lido!");
+    }
+}
