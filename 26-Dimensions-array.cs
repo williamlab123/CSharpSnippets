6928d37c4c5edb93d4b2464d8f48fe5383afbc4a
William Pedro <williamprv@outlook.com>
2023-04-23 00:42:22 -0300

    Create 26-Dimensions-array.cs

---
+++ 26-Dimensions-array.cs
@@ -0,0 +1,166 @@
+ int[,,,,,,,,,,,,,,,,,,,,,,,,,] matriz = new int[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+
+        Random rand = new Random();
+
+        for (int a = 0; a < 2; a++)
+        {
+            for (int b = 0; b < 2; b++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    for (int d = 0; d < 2; d++)
+                    {
+                        for (int e = 0; e < 2; e++)
+                        {
+                            for (int f = 0; f < 2; f++)
+                            {
+                                for (int g = 0; g < 2; g++)
+                                {
+                                    for (int h = 0; h < 2; h++)
+                                    {
+                                        for (int i = 0; i < 2; i++)
+                                        {
+                                            for (int j = 0; j < 2; j++)
+                                            {
+                                                for (int k = 0; k < 2; k++)
+                                                {
+                                                    for (int l = 0; l < 2; l++)
+                                                    {
+                                                        for (int m = 0; m < 2; m++)
+                                                        {
+                                                            for (int n = 0; n < 2; n++)
+                                                            {
+                                                                for (int o = 0; o < 2; o++)
+                                                                {
+                                                                    for (int p = 0; p < 2; p++)
+                                                                    {
+                                                                        for (int q = 0; q < 2; q++)
+                                                                        {
+                                                                            for (int r = 0; r < 2; r++)
+                                                                            {
+                                                                                for (int s = 0; s < 2; s++)
+                                                                                {
+                                                                                    for (int t = 0; t < 2; t++)
+                                                                                    {
+                                                                                        for (int u = 0; u < 2; u++)
+                                                                                        {
+                                                                                            for (int v = 0; v < 2; v++)
+                                                                                            {
+                                                                                                for (int w = 0; w < 2; w++)
+                                                                                                {
+                                                                                                    for (int x = 0; x < 2; x++)
+                                                                                                    {
+                                                                                                        for (int y = 0; y < 2; y++)
+                                                                                                        {
+                                                                                                            for (int z = 0; z < 2; z++)
+                                                                                                            {
+                                                                                                                matriz[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z] = rand.Next(100);
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+      
+        for (int a = 0; a < 2; a++)
+        {
+            for (int b = 0; b < 2; b++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    for (int d = 0; d < 2; d++)
+                    {
+                        for (int e = 0; e < 2; e++)
+                        {
+                            for (int f = 0; f < 2; f++)
+                            {
+                                for (int g = 0; g < 2; g++)
+                                {
+                                    for (int h = 0; h < 2; h++)
+                                    {
+                                        for (int i = 0; i < 2; i++)
+                                        {
+                                            for (int j = 0; j < 2; j++)
+                                            {
+                                                for (int k = 0; k < 2; k++)
+                                                {
+                                                    for (int l = 0; l < 2; l++)
+                                                    {
+                                                        for (int m = 0; m < 2; m++)
+                                                        {
+                                                            for (int n = 0; n < 2; n++)
+                                                            {
+                                                                for (int o = 0; o < 2; o++)
+                                                                {
+                                                                    for (int p = 0; p < 2; p++)
+                                                                    {
+                                                                        for (int q = 0; q < 2; q++)
+                                                                        {
+                                                                            for (int r = 0; r < 2; r++)
+                                                                            {
+                                                                                for (int s = 0; s < 2; s++)
+                                                                                {
+                                                                                    for (int t = 0; t < 2; t++)
+                                                                                    {
+                                                                                        for (int u = 0; u < 2; u++)
+                                                                                        {
+                                                                                            for (int v = 0; v < 2; v++)
+                                                                                            {
+                                                                                                for (int w = 0; w < 2; w++)
+                                                                                                {
+                                                                                                    for (int x = 0; x < 2; x++)
+                                                                                                    {
+                                                                                                        for (int y = 0; y < 2; y++)
+                                                                                                        {
+                                                                                                            for (int z = 0; z < 2; z++)
+                                                                                                            {
+                                                                                                                Console.Write(matriz[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z] + " ");
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
