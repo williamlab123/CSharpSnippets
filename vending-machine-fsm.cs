9b0b9beb0805e6ab3d9eb4206fd80ff497d3fe98
William Pedro <williamprv@outlook.com>
2023-05-01 13:32:32 -0300

    Create vending-machine-fsm.cs
    This C# code demonstrates the concept of finite-state machines. It uses an enum to represent different states and methods to define the possible transitions between states. The main purpose of the code is to simulate a vending machine that dispenses a product when the user inserts a coin and selects a product.

---
+++ vending-machine-fsm.cs
@@ -0,0 +1,78 @@
+using System;
+
+public enum VendingMachineState
+{
+    WaitingForSelection,
+    ProcessingSelection,
+    Vending
+}
+
+public class VendingMachine
+{
+    private VendingMachineState currentState;
+
+    public VendingMachine()
+    {
+        currentState = VendingMachineState.WaitingForSelection;
+    }
+
+    public void InsertCoin()
+    {
+        if (currentState == VendingMachineState.WaitingForSelection)
+        {
+            Console.WriteLine("Coin inserted");
+            currentState = VendingMachineState.ProcessingSelection;
+        }
+        else
+        {
+            Console.WriteLine("Invalid action");
+        }
+    }
+
+    public void SelectProduct()
+    {
+        if (currentState == VendingMachineState.ProcessingSelection)
+        {
+            Console.WriteLine("Product selected");
+            currentState = VendingMachineState.Vending;
+        }
+        else
+        {
+            Console.WriteLine("Invalid action");
+        }
+    }
+
+    public void DispenseProduct()
+    {
+        if (currentState == VendingMachineState.Vending)
+        {
+            Console.WriteLine("Product dispensed");
+            currentState = VendingMachineState.WaitingForSelection;
+        }
+        else
+        {
+            Console.WriteLine("Invalid action");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        VendingMachine machine = new VendingMachine();
+
+        machine.InsertCoin(); // Coin inserted
+
+        machine.InsertCoin(); // Invalid action (the machine is already processing)
+
+        machine.SelectProduct(); // Product selected
+
+        machine.InsertCoin(); // Invalid action (the machine is waiting to dispense)
+
+        machine.SelectProduct(); // Invalid action (the product is already selected)
+
+        machine.DispenseProduct(); // Product dispensed
+
+    }
+}
